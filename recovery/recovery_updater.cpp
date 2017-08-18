e7762992d2ad9a485a14c9fa1108d8ec5bfff87c
Luca Stefani <luca020400@lineageos.org>
2017-08-18 14:22:16 +0200

    oneplus3: recovery: Update for O
    Change-Id: Ie1ca664348726c0e81e689a1b5a6b67fad75f1f1

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -27,8 +27,10 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <string>
+#include <vector>
+
 #include "edify/expr.h"
-#include "updater/install.h"
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
@@ -162,9 +164,9 @@ err_ret:
 }
 
 /* verify_modem("MODEM_VERSION") */
-Value * VerifyModemFn(const char *name, State *state, int argc, Expr *argv[]) {
+Value* VerifyModemFn(const char* name, State* state,
+                      const std::vector<std::unique_ptr<Expr>>& argv) {
     char current_modem_version[MODEM_VER_BUF_LEN];
-    char* modem_version;
     int ret;
     struct tm tm1, tm2;
 
@@ -177,21 +179,19 @@ Value * VerifyModemFn(const char *name, State *state, int argc, Expr *argv[]) {
     memset(&tm1, 0, sizeof(tm));
     strptime(current_modem_version, "%Y-%m-%d %H:%M:%S", &tm1);
 
-    ret = ReadArgs(state, argv, 1, &modem_version);
-    if (ret < 0) {
+    std::vector<std::string> args;
+    if (!ReadArgs(state, argv, &args)) {
         return ErrorAbort(state, kArgsParsingFailure, "%s() error parsing arguments", name);
     }
 
-    uiPrintf(state, "Checking for MODEM build time-stamp %s\n", modem_version);
+    for (auto& modem_version : args) {
+      memset(&tm2, 0, sizeof(tm));
+      strptime(modem_version.c_str(), "%Y-%m-%d %H:%M:%S", &tm2);
 
-    memset(&tm2, 0, sizeof(tm));
-    strptime(modem_version, "%Y-%m-%d %H:%M:%S", &tm2);
-
-    if (mktime(&tm1) >= mktime(&tm2)) {
-        ret = 1;
+      if (mktime(&tm1) >= mktime(&tm2)) {
+          ret = 1;
+      }
     }
-
-    free(modem_version);
 
     return StringValue(strdup(ret ? "1" : "0"));
 }
