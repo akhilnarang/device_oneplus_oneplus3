9092a86dc90ef42e1c1620bea64e727b3a35621d
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:23:30 +0200

    oneplus3: Add HIDL manifest
     * This defines the interfaces for graphics and wifi
    
    Change-Id: I2c3aece8ad13613ec5b83496aa23d26ef238f45d

--- device.mk
+++ device.mk
@@ -231,6 +231,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
 
+# HIDL manifest
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
