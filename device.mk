6ccd67136e345cae761d8b530887232bd17c9ba7
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:10:19 +0200

    oneplus3: Build DRM HIDL HAL
    Change-Id: I26ddf6d07457b1a22ea6b60c77e62ec2260ee5a9

--- device.mk
+++ device.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
