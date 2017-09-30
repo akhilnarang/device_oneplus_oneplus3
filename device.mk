ec299301d94d15cfd164aa54f5936e74902f296a
maxwen <max.weninger@gmail.com>
2017-09-30 02:36:33 +0200

    op3(t): disable camera hidl service
    Change-Id: I9d1dbf014f12df67421285d0535d01e078c98bde

--- device.mk
+++ device.mk
@@ -158,8 +158,6 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    camera.device@1.0-impl \
     camera.device@3.2-impl \
     Snap \
     libcamera_shim
