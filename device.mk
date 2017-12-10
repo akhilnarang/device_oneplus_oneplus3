e9d41bc430aa2e7e7c493f9c5d96363b9b5b21ed
Akhil Narang <akhilnarang.1999@gmail.com>
2017-12-10 18:15:31 +0530

    Build opdoze and pocketmode
    Change-Id: I451248156bb89dc32325d94762cdbcac196f16a4
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -203,15 +203,15 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
-# Doze mode
-#PRODUCT_PACKAGES += \
-#    OneplusDoze
+ Doze mode
+PRODUCT_PACKAGES += \
+    OneplusDoze
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
-    fingerprint.msm8996
-#    OneplusPocketMode
+    fingerprint.msm8996 \
+    OneplusPocketMode
 
 # For config.fs
 PRODUCT_PACKAGES += \
