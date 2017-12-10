c35f056c1fa83657dda0481932e10e5153be5514
Akhil Narang <akhilnarang.1999@gmail.com>
2017-12-10 18:23:15 +0530

    Akhil what are you trying to do?
    Change-Id: Id0d8a13f8608288332fcac80edf983abe53004b9
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -203,7 +203,7 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
- Doze mode
+# Doze mode
 PRODUCT_PACKAGES += \
     OneplusDoze
 
