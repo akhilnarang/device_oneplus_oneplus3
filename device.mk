429e6771a0502763f4009a16a12f7151a59dd35f
Jagrav Naik <jagravnaik0@gmail.com>
2018-02-11 17:22:42 +0100

    op3: build SnapdragonCamera
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -194,7 +194,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
+    SnapdragonCamera \
     libcamera_parameters_shim \
     libcamera_shim \
     camera.device@1.0-impl \
